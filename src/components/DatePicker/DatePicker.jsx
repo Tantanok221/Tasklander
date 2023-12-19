ede8e444dc83504390b94a46c0ff7c252b3c2473
Tan Kai <6572withspicy@gmail.com>
2023-12-19 21:38:50 +0800

    chore: backup to fix local vite issue

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-import { getWeek, getYear, getMonth, getQuarter,format } from "date-fns";
+import {
+  getWeek,
+  getDay,
+  getYear,
+  getWeekOfMonth,
+  startOfMonth,
+  getDate,
+  getMonth,
+  getQuarter,
+  format,
+  eachDayOfInterval,
+  getDaysInMonth,
+  eachWeekOfInterval,
+  getDayOfYear,
+} from "date-fns";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import style from "./style.module.scss";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
@@ -7,13 +21,28 @@ import { IconContext } from "react-icons";
 
 const DatePicker = () => {
   const today = new Date();
-  const week = getWeek(today);
-  const month = format(today, "MMMM")
-  const year = getYear(today);
-  const quater = getQuarter(today)
-  const [date, setDate] = React.useState(week);
-  let Quater = ["Q1", "Q2", "Q3", "Q4"];
-  let nowWeekArray = [];
+  const nowDay = getDate(today);
+  const nowWeek = getWeek(today);
+  const nowMonth = getMonth(today);
+  const nowFormatMonth = format(today, "MMMM");
+  const nowYear = getYear(today);
+  const AllOfThisYear = eachDayOfInterval({
+    start: new Date(nowYear, 0, 1),
+    end: new Date(nowYear, 11, 31),
+  });
+  const todayIndex = getDayOfYear(today) - 1;
+  const StartOfThisMonthIndex = todayIndex - nowDay + 1;
+  const totalDate = getDaysInMonth(today);
+  let DayOfWeek = getDay(AllOfThisYear[todayIndex]);
+  if (DayOfWeek === 0) DayOfWeek = 7;
+  console.log(DayOfWeek)
+  // 0 represent sunday, 1 represent monday so on and so forth
+  const [date, setDate] = React.useState("");
+  let globalIterate = 0;
+  
+  let ArrayOfQuater = ["Q1", "Q2", "Q3", "Q4"];
+  let ArrayOfWeek = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+  
   return (
     <ToggleGroup.Root
       type="single"
@@ -26,12 +55,16 @@ const DatePicker = () => {
       <IconContext.Provider value={{ color: "#686868", size: "1.5rem" }}>
         <div className={`${style.row}`}>
           <FaAngleLeft className={style.icon} />
-          <ToggleGroup.Item value={year} className={style.button}>
-            <div className={`${style.highlight} ${style.text}`}>{year}</div>
+          <ToggleGroup.Item value={nowYear} className={style.button}>
+            <div className={`${style.highlight} ${style.text}`}>{nowYear}</div>
           </ToggleGroup.Item>
-          {Quater.map((item, index) => {
+          {ArrayOfQuater.map((item, index) => {
             return (
-              <ToggleGroup.Item value={item} className={style.button}>
+              <ToggleGroup.Item
+                key={index}
+                value={item}
+                className={style.button}
+              >
                 <div className={`${style.nonHighlight} ${style.text}`}>
                   {item}
                 </div>
@@ -42,14 +75,27 @@ const DatePicker = () => {
         </div>
         <div className={`${style.row}`}>
           <FaAngleLeft className={style.icon} />
-          <div className={`${style.highlight} ${style.text}`}>{month}</div>
+          <ToggleGroup.Item
+            value={nowFormatMonth}
+            className={`${style.highlight} ${style.text} ${style.button}`}
+          >
+            {nowFormatMonth}
+          </ToggleGroup.Item>
           <FaAngleRight className={style.icon} />
         </div>
-        <div className={style.row}>
-          <div className={style.week}>
-            <div className={`${style.gray} ${style.text}`}>WEEKS</div>
+        <div className={`${style.row}`}>
+          <div className={style.day}>
+            <div className={style.dayRow}>
+              {ArrayOfWeek.map((item, index) => {
+                return (
+                  <div key={index} className={`${style.text} ${style.gray}`}>
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
+            
           </div>
-          <div className={style.day}></div>
         </div>
       </IconContext.Provider>
     </ToggleGroup.Root>
