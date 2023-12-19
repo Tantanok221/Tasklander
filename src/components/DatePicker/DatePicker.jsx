66cf736a49d61e6f805c4f96d01c68754d3c4a06
Tan Kai <6572withspicy@gmail.com>
2023-12-19 15:49:36 +0800

    style: added first & second row of date picker, third row still WIP

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,37 +1,57 @@
 import React from "react";
-import { getWeek, getYear } from "date-fns";
+import { getWeek, getYear, getMonth, getQuarter,format } from "date-fns";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
-import style from "./style.module.scss"
-import { FaAngleLeft,FaAngleRight } from "react-icons/fa";
+import style from "./style.module.scss";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const DatePicker = () => {
-  const year = getYear(new Date());
-  const today = getWeek(new Date());
-  const [date, setDate] = React.useState(today);
-  let Quater = ["Q1", "Q2", "Q3", "Q4"]
+  const today = new Date();
+  const week = getWeek(today);
+  const month = format(today, "MMMM")
+  const year = getYear(today);
+  const quater = getQuarter(today)
+  const [date, setDate] = React.useState(week);
+  let Quater = ["Q1", "Q2", "Q3", "Q4"];
+  let nowWeekArray = [];
   return (
     <ToggleGroup.Root
       type="single"
       value={date}
+      className={style.body}
       onValueChange={(date) => {
         if (date) setDate(date);
       }}
     >
-      <div className={style.row1}>
-      <FaAngleLeft/>
-      <ToggleGroup.Item value={year}>
-        <div className={style.highlight} >{year}</div>
-      </ToggleGroup.Item>
-      {Quater.map((item, index) => {
-        return(
-          <ToggleGroup.Item value={item}>
-            <div className={style.nonHighlight} >{item}</div>
+      <IconContext.Provider value={{ color: "#686868", size: "1.5rem" }}>
+        <div className={`${style.row}`}>
+          <FaAngleLeft className={style.icon} />
+          <ToggleGroup.Item value={year} className={style.button}>
+            <div className={`${style.highlight} ${style.text}`}>{year}</div>
           </ToggleGroup.Item>
-
-        )
-      })}
-      <FaAngleRight/>
-      </div>
+          {Quater.map((item, index) => {
+            return (
+              <ToggleGroup.Item value={item} className={style.button}>
+                <div className={`${style.nonHighlight} ${style.text}`}>
+                  {item}
+                </div>
+              </ToggleGroup.Item>
+            );
+          })}
+          <FaAngleRight className={style.icon} />
+        </div>
+        <div className={`${style.row}`}>
+          <FaAngleLeft className={style.icon} />
+          <div className={`${style.highlight} ${style.text}`}>{month}</div>
+          <FaAngleRight className={style.icon} />
+        </div>
+        <div className={style.row}>
+          <div className={style.week}>
+            <div className={`${style.gray} ${style.text}`}>WEEKS</div>
+          </div>
+          <div className={style.day}></div>
+        </div>
+      </IconContext.Provider>
     </ToggleGroup.Root>
   );
 };
