1ffad8cf0188d25fca67676fec5d9be6cf5cffd0
Tan Kai <6572withspicy@gmail.com>
2023-12-30 12:25:24 +0800

    feat: Added active selection logic for date selector

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -188,7 +188,7 @@ const DatePicker = ({ globalDate, setGlobalDate }) => {
                         <ToggleGroup.Item
                           value={new Date(nowYear, getMonth(today), item)}
                           key={"date "+nowDay+nowFormatMonth+nowYear + i}
-                          className={sx("button", "textContainer", "red")}
+                          className={sx("button", "textContainer", "red", {"active": globalDateWithoutTime === format(new Date(nowYear, getMonth(today), item), "yyyy-MM-dd")} )}
                         >
                           <div className={sx("gray")}>{item}</div>
                         </ToggleGroup.Item>
@@ -200,7 +200,7 @@ const DatePicker = ({ globalDate, setGlobalDate }) => {
                       <ToggleGroup.Item
                         value={new Date(nowYear, getMonth(today), item)}
                         key={"dateNonHighlight "+nowDay+nowFormatMonth+nowYear + i}
-                        className={sx("button", "textContainer")}
+                        className={sx("button", "textContainer",{"active": globalDateWithoutTime === format(new Date(nowYear, getMonth(today), item), "yyyy-MM-dd")})}
                       >
                         <div className={sx("nonHighlight")}>{item}</div>
                       </ToggleGroup.Item>
@@ -214,9 +214,9 @@ const DatePicker = ({ globalDate, setGlobalDate }) => {
         <div className={sx("row")}>
           <div
             onClick={() => {
-              setGlobalDate(getYear(new Date()),
+              setGlobalDate(new Date(getYear(new Date()),
               getMonth(new Date()),
-              getDate(new Date()));
+              getDate(new Date())));
               // To avoid having time signature when returning back to GlobalDate
             }}
             className={sx("quickSelect", {"active": globalDateWithoutTime === format(new Date(nowYear, getMonth(today), nowDay), "yyyy-MM-dd")} )}
