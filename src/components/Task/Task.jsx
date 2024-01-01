0ba4b20a8c0b8fd8ca9f0c1c681e6beb2aec585c
Tan Kai <6572withspicy@gmail.com>
2024-01-01 12:13:30 +0800

    feat: Initialize Task Component

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./style.module.css"
+import classNames from 'classnames/bind'
+const Task = () => {
+  return (
+    <div>Task</div>
+  )
+}
+
+export default Task
