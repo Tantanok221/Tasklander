030ee560329b2a5283d4cbc07a55a8d081e59e50
Tantanok221 <6572withspicy@gmail.com>
2024-01-04 15:19:44 +0800

    feat: Added authenthication function

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import style from "./style.module.css"
 import classNames from 'classnames/bind'
+import {motion} from "framer-motion"
+import {supabase} from "../../helper/supabase.js"
+
+
+async function signInWithEmail(email,password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
+  })
+}
+
+
 const Login = () => {
+  const sx = classNames.bind(style)
   return (
     <div>Task</div>
   )
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import style from "./style.module.css"
 import classNames from 'classnames/bind'
+import {motion} from "framer-motion"
+import {supabase} from "../..helper/supabase.js"
+
+async function signUpNewUser(email,password) {
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+    options: {
+      emailRedirectTo: 'https//example.com/welcome'
+    }
+  })
+}
+
 const SignUp = () => {
+  const sx = classNames.bind(style)
   return (
     <div>SignUp</div>
   )
