7f3efda47dc0d07bba7c9aafdd9e2f755e84c850
Tantanok221 <6572withspicy@gmail.com>
2024-01-04 15:13:05 +0800

    chore: Initialize Component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./style.module.css"
+import classNames from 'classnames/bind'
+const Login = () => {
+  return (
+    <div>Task</div>
+  )
+}
+
+export default Login
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from "./style.module.css"
+import classNames from 'classnames/bind'
+const SignUp = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+export default SignUp
