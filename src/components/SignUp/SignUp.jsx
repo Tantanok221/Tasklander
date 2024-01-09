94a55c195d92b571e4a493d62e0147a098984af5
Tan Kai <6572withspicy@gmail.com>
2024-01-09 23:04:25 +0800

    feat: Added Sign Up Button & Error Handling

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -6,18 +6,24 @@ import { supabase } from "../../helper/supabase.js";
 import { FcGoogle } from "react-icons/fc";
 import * as Form from "@radix-ui/react-form";
 import { IconContext } from "react-icons";
+import isEmail from "validator/lib/isEmail";
+
 async function signUpNewUser(email, password) {
-  const { data, error } = await supabase.auth.signUp({
+  const message = await supabase.auth.signUp({
     email: email,
     password: password,
-    options: {
-      emailRedirectTo: "https//example.com/welcome",
-    },
   });
+  return message;
 }
-
 const SignUp = () => {
+  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState([]);
+  const [error, setError] = React.useState([]);
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+
   const sx = classNames.bind(style);
+  console.log(error);
   return (
     <div className={sx("container")}>
       <div className={sx("header")}>
@@ -40,24 +46,69 @@ const SignUp = () => {
         <Form.Field className={sx("formField")}>
           <Form.Label className={sx("formLabel")}>Email</Form.Label>
           <Form.Control asChild>
-            <input placeholder="Enter Your Email" className={sx("input")} type="email" required />
+            <input
+              placeholder="Enter Your Email"
+              onChange={(value) => setEmail(value.currentTarget.value)}
+              className={sx("input", { errorBorder: emailError[0] })}
+              type="email"
+              required
+            />
           </Form.Control>
-          <Form.Message className={sx("formMessage")} match="typeMismatch">
+          <Form.Message
+            className={sx("formMessage", {
+              errorText: emailError[1]?.reasons == "invalid",
+            })}
+            match="typeMismatch"
+          >
             Please provide a valid email
           </Form.Message>
         </Form.Field>
         <Form.Field className={sx("formField")}>
           <Form.Label className={sx("formLabel")}>Password</Form.Label>
           <Form.Control asChild>
-            <input placeholder="Enter Your Password"  minlength="6" className={sx("input")} type="password" required />
+            <input
+              placeholder="Enter Your Password"
+              id="password"
+              onChange={(value) => setPassword(value.currentTarget.value)}
+              minLength="6"
+              className={sx("input", { errorBorder: passwordError[0] })}
+              type="password"
+              required
+            />
           </Form.Control>
-          <Form.Message className={sx("formMessage")} match="tooShort">
-          The password must be longer than 6 character
-          </Form.Message>
-          <Form.Message className={sx("formMessage")} match="typeMismatch">
-            Please provide a valid password
+          <Form.Message
+            className={sx("formMessage", {
+              errorText: passwordError[1]?.reasons == "length",
+            })}
+            match="tooShort"
+          >
+            The password must be longer than 6 character
           </Form.Message>
         </Form.Field>
+        <Form.Submit asChild>
+          <button
+            onClick={() => {
+              if (!isEmail(email)) {
+                setEmailError([true, { reasons: "invalid" }]);
+                return false;
+              }
+              if (password.length < 6) {
+                setPasswordError([true, { reasons: "length" }]);
+                return false;
+              }
+              if (isEmail(email) && password.length > 6) {
+                setTimeout(async () => {
+                  let message = await signUpNewUser(email, password);
+                  console.log(message);
+                  setError(message.error);
+                }, 0);
+              }
+            }}
+            className={sx("formButton")}
+          >
+            Sign Up
+          </button>
+        </Form.Submit>
       </Form.Root>
     </div>
   );
