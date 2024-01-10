e96388d0a41732749b3c3f78e13d4187063fe276
Tan Kai <6572withspicy@gmail.com>
2024-01-10 22:46:27 +0800

    feat: Added Show Password Checkbox & better error handling

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -7,6 +7,8 @@ import { FcGoogle } from "react-icons/fc";
 import * as Form from "@radix-ui/react-form";
 import { IconContext } from "react-icons";
 import isEmail from "validator/lib/isEmail";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { FaCheck } from "react-icons/fa6";
 
 async function signUpNewUser(email, password) {
   const message = await supabase.auth.signUp({
@@ -15,15 +17,16 @@ async function signUpNewUser(email, password) {
   });
   return message;
 }
+
 const SignUp = () => {
   const [password, setPassword] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [error, setError] = React.useState("");
   const [emailError, setEmailError] = React.useState([]);
   const [passwordError, setPasswordError] = React.useState([]);
-
+  const [visible, setVisible] = React.useState(false);
+  console.log(error.name);
   const sx = classNames.bind(style);
-  console.log(error);
   return (
     <div className={sx("container")}>
       <div className={sx("header")}>
@@ -42,14 +45,21 @@ const SignUp = () => {
         <span className={sx("or")}>OR</span>
         <div className={sx("line")}></div>
       </div>
-      <Form.Root className={sx("formContainer")}>
+      <Form.Root
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        className={sx("formContainer")}
+      >
         <Form.Field className={sx("formField")}>
           <Form.Label className={sx("formLabel")}>Email</Form.Label>
           <Form.Control asChild>
             <input
               placeholder="Enter Your Email"
               onChange={(value) => setEmail(value.currentTarget.value)}
-              className={sx("input", { errorBorder: emailError[0] })}
+              className={sx("input", {
+                errorBorder: emailError[0] || error.name,
+              })}
               type="email"
               required
             />
@@ -63,9 +73,17 @@ const SignUp = () => {
             >
               Please provide a valid email
             </div>
-          ) : (
-            <></>
-          )}
+          ) : null}
+          {error.message === "User already registered" ? (
+            <div
+              className={sx("formMessage", {
+                errorText: emailError[1]?.reasons == "invalid" || error?.name,
+              })}
+              match="typeMismatch"
+            >
+              User Already Registerd
+            </div>
+          ) : null}
         </Form.Field>
         <Form.Field className={sx("formField")}>
           <Form.Label className={sx("formLabel")}>Password</Form.Label>
@@ -76,7 +94,7 @@ const SignUp = () => {
               onChange={(value) => setPassword(value.currentTarget.value)}
               minLength="6"
               className={sx("input", { errorBorder: passwordError[0] })}
-              type="password"
+              type={visible ? "text" : "password"}
               required
             />
           </Form.Control>
@@ -93,6 +111,18 @@ const SignUp = () => {
             <></>
           )}
         </Form.Field>
+        <Checkbox.Root onCheckedChange={() => {
+          setVisible(!visible)
+        }} className={sx("checkboxContainer")}>
+          <div className={sx("checkbox")}>
+            <Checkbox.Indicator>
+            <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+              <FaCheck />
+            </IconContext.Provider>
+            </Checkbox.Indicator>
+          </div>
+          <label className={sx("checkboxLabel")}>Show Password</label>
+        </Checkbox.Root>
         <Form.Submit asChild>
           <button
             onClick={() => {
