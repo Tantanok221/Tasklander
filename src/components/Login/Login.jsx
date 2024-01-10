bb4db225f0ddde7c0f66a3700b1719eaacb06cd3
Tan Kai <6572withspicy@gmail.com>
2024-01-10 08:44:24 +0800

    chore(prettier): Formatted with prettier

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import style from "./style.module.css"
-import classNames from 'classnames/bind'
-import {motion} from "framer-motion"
-import {supabase} from "../../helper/supabase.js"
+import React from "react";
+import style from "./style.module.css";
+import classNames from "classnames/bind";
+import { motion } from "framer-motion";
+import { supabase } from "../../helper/supabase.js";
 
-
-async function signInWithEmail(email,password) {
+async function signInWithEmail(email, password) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
-    password: password
-  })
+    password: password,
+  });
 }
-
 
 const Login = () => {
-  const sx = classNames.bind(style)
-  return (
-    <div>Task</div>
-  )
-}
+  const sx = classNames.bind(style);
+  return <div>Task</div>;
+};
 
-export default Login
+export default Login;
