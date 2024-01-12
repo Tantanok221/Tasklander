338e511d0316ca5729c1d91536bb53264678aea6
Tan Kai <6572withspicy@gmail.com>
2024-01-12 16:57:59 +0800

    feat: Added signInWithEmail function

--- src/helper/supabase.js
+++ src/helper/supabase.js
@@ -1,2 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(import.meta.env.VITE_LINK, import.meta.env.VITE_PUBLIC_KEY )
+
+export async function signInWithEmail(email, password) {
+  const message = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+  return message;
+}
