75f4f886ac8ac7702ca807299a35bda2765b19ae
Tan Kai <6572withspicy@gmail.com>
2024-01-12 16:58:23 +0800

    refactor: Refactor header to its own subcomponent

---
+++ src/components/Login/subcomponent/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "../style.module.css";
+import classNames from "classnames/bind";
+
+export const Header = () => {
+  const sx = classNames.bind(style);
+  return (
+    <div className={sx("header")}>
+        <h1 className={sx("title")}>Login</h1>
+        <span className={sx("subtitle")}>Login To Your Account</span>
+    </div>
+  )
+}
